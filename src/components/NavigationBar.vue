0e71e4a0ad82cdca344bfaed0c30726bc3f7cd34
Calin <cotuna.calin@yahoo.com>
2024-09-26 15:39:07 +0300

    Style - Created a temporary navbar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 <template>
-    <div>
-        Import text
+    <div class="p-3 w-full border-b">
+        <span class="p-3 text-xl font-medium">Import text</span>
     </div>
 </template>
 <style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,8 @@
 
 <template>
   <main>
-    <div class="flex flex-col space-y-5 items-center justify-center px-8 text-center text-xl">
-      <p class="px-8 text-gray-400 text-md italic">Adauga primul tau text, prin una din cele 3 metode</p>
+    <div class="flex flex-col space-y-5 items-center justify-center px-8 mt-5 text-center text-xl">
+      <p class="px-8 text-gray-400 text-md italic">Adaugǎ primul tǎu text, prin una din cele 3 metode</p>
       <div class="flex flex-col space-y-6 px-10 py-5 border border-green-700 rounded-md">
         <button class="flex items-center space-x-8 px-8 py-2 text-white bg-brand-olivine rounded-full">
           <i class="bi bi-pencil-square text-3xl"></i>
@@ -12,14 +12,14 @@
         </button>
         <button class="flex items-center space-x-8 px-8 py-2 text-white bg-brand-olivine rounded-full">
           <i class="bi bi-file-earmark-arrow-up text-3xl"></i>
-          <span class="text-xl">Import fisier</span>
+          <span class="text-xl">Import fișier</span>
         </button>
         <label class="inline-flex items-center cursor-pointer">
           <input type="checkbox" value="" class="sr-only peer">
           <div
             class="relative w-11 h-6 bg-gray-400 peer-focus:outline-none rounded-full peer peer-checked:bg-brand-olivine peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all">
           </div>
-          <span class="ms-3 text-sm text-gray-900">Optiuni predefinite de import</span>
+          <span class="ms-3 text-sm text-gray-900">Opțiuni predefinite de import</span>
         </label>
       </div>
       <button class="flex items-center space-x-8 px-5 py-2 text-white bg-brand-olivine rounded-full">
