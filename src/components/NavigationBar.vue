a1df1f10b74eb26835883d7062bd3e7b5607a7a5
Calin <cotuna.calin@yahoo.com>
2024-09-27 21:11:10 +0300

    Style - created library view - card designs

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -25,16 +25,22 @@ const goBack = () => {
 };
 </script>
 <template>
-    <div class="p-3 w-full flex justify-between border-b ">
+    <!-- <RouterLink :to="{name: 'biblioteca'}">Biblioteca</RouterLink> -->
+    <div class="p-3 w-full flex justify-between items-center border-b ">
         <div>
-            <button v-if="route.path != '/'" @click="goBack" class="text-lg text-gray-500 hover:text-gray-800">
-            <i class="bi bi-chevron-left text-brand-olivine"></i>
-        </button>
-        <span class="p-3 text-xl font-medium">{{ navbarTitle }}</span>
+            <button v-if="route.path !== '/' && route.path !== '/biblioteca'" @click="goBack"
+                class="text-lg text-gray-500 hover:text-gray-800">
+                <i class="bi bi-chevron-left text-brand-olivine"></i>
+            </button>
+            <span class="p-3 text-2xl font-medium">{{ navbarTitle }}</span>
         </div>
-        <div>
-            <RouterLink :to="{name: 'biblioteca'}">Biblioteca</RouterLink>
+        <div v-if="route.path !== '/import-text' && route.path !== '/import-clasificat' " class="flex space-x-3 text-3xl">
+            <i class="bi bi-search"></i>
+            <i class="bi bi-bell-fill"></i>
+            <i class="bi bi-plus-circle-fill text-brand-olivine"></i>
+            <i class="bi bi-gear"></i>
         </div>
     </div>
+
 </template>
 <style scoped></style>
