566e6785d220a095b14d608106cb091b0af2ad92
Calin <cotuna.calin@yahoo.com>
2024-09-30 22:49:42 +0300

    Style - created temp structure for private and public translations buttons

--- src/views/BibleText.vue
+++ src/views/BibleText.vue
@@ -7,8 +7,9 @@ const toggleContent = () => {
 </script>
 <template>
     Text biblic
-    <div class="relative bg-brand-honeydew p-3">
-        <p class="text-xl"><span class=" text-sm text-gray-500">4 </span>Cât priveşte legăturile cu oamenii, eu, după
+    <div class="relative">
+        <p class="text-xl bg-brand-honeydew p-3"><span class=" text-sm text-gray-500">4 </span>Cât priveşte legăturile
+            cu oamenii, eu, după
             cuvântul buzelor Tale, mă feresc de calea celor
             asupritori;</p>
 
@@ -23,13 +24,53 @@ const toggleContent = () => {
             class="bi bi-bell-fill bg-white text-brand-gold-metallic rounded-full p-2 flex items-center justify-center absolute -bottom-9 left-24 w-12 h-12 text-2xl"></i>
         <i
             class="bi bi-plus-circle-fill bg-white text-brand-olivine rounded-full p-2 flex items-center justify-center absolute -bottom-9 right-4 w-12 h-12 text-3xl"></i>
-
-        
     </div>
-    <div v-if="isOpen" class="mt-12 p-3 bg-gray-200 rounded">
-            <p>Duis ultrices accumsan lorem, non ultricies dolor dapibus sit amet. Praesent dignissim finibus tempus.
-            </p>
+    <div v-if="isOpen" class="border-s-8 border-brand-olivine border-h mt-12  rounded">
+        <div class="relative">
+            <p class="bg-brand-cornsilk p-3">Duis ultrices accumsan lorem, non ultricies dolor dapibus sit amet.
+                Praesent dignissim finibus tempus.</p>
+            <div class="absolute -bottom-9 left-4 flex items-center">
+                <div class="bg-white rounded-full w-12 h-12 flex items-center justify-center">
+                    <div
+                        class="bg-white rounded-full w-10 h-10 flex items-center justify-center border border-brand-olivine">
+                        <span class="font-medium text-brand-olivine">FG</span>
+                    </div>
+
+                </div>
+                <p class="text-gray-400 p-2 pt-4">Acum 14 minute</p>
+            </div>
+
+
+            <div class="absolute -bottom-9 right-0 bg-white rounded-s-full w-[6.5rem] h-12">
+                <button
+                    class="absolute bottom-1 right-0 rounded-s-full py-1 w-24 bg-white border-brand-gold-metallic border text-brand-gold-metallic text-xl">Privat</button>
+            </div>
         </div>
+
+        <div class="relative mt-8">
+            <p class="p-3">Duis ultrices accumsan lorem, non ultricies dolor dapibus sit amet. Praesent dignissim
+                finibus tempus. </p>
+
+            <div class="absolute -bottom-9 left-4 flex items-center">
+                <div class="bg-white rounded-full w-12 h-12 flex items-center justify-center">
+                    <div
+                        class="bg-white rounded-full w-10 h-10 flex items-center justify-center border border-brand-olivine">
+                        <span class="font-medium text-brand-olivine">DR</span>
+                    </div>
+
+                </div>
+                <p class="text-gray-400 p-2 pt-4">Acum 3 h</p>
+            </div>
+
+
+            <div class="absolute -bottom-9 right-0 bg-white rounded-s-full w-[6.5rem] h-12">
+                <i
+                    class="bi bi-pen bg-white shadow-md text-brand-gold-metallic rounded-full p-2 flex items-center justify-center absolute bottom-1 right-28 w-10 h-10 text-2xl"></i>
+                <button
+                    class="absolute bottom-1 right-0 rounded-s-full py-1 w-24 bg-white border-brand-gold-metallic border text-brand-gold-metallic text-xl">Privat</button>
+            </div>
+        </div>
+    </div>
 
     <div class="mt-16 p-3 relative">
         <p class="text-xl"><span class="text-sm text-gray-500">5 </span>paşii mei stau neclintiţi* pe cărările Tale şi
