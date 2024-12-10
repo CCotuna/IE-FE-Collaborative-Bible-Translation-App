f8abf483d4638d83da5df737657af5a783017f09
Calin <cotuna.calin@yahoo.com>
2024-12-10 11:31:06 +0200

    Removed unnecessary div

--- src/components/ui/BaseImportForm.vue
+++ src/components/ui/BaseImportForm.vue
@@ -32,31 +32,29 @@ const submitForm = () => {
 </script>
 
 <template>
-  <div class="flex flex-col p-8 pt-2 ">
-    <form @submit.prevent="submitForm">
-      <div class="flex space-x-2 mb-4">
-        <span class="px-2 py-1 text-xl bg-gray-200 rounded-md">
-          <i class="bi bi-type-bold"></i>
-        </span>
-        <span class="px-2 py-1 text-xl bg-gray-200 rounded-md">
-          <i class="bi bi-type-underline"></i>
-        </span>
-        <span class="px-2 py-1 text-xl bg-gray-200 rounded-md">
-          <i class="bi bi-type-italic"></i>
-        </span>
-      </div>
+  <form class="flex flex-col p-8 pt-2 " @submit.prevent="submitForm">
+    <div class="flex space-x-2 mb-4">
+      <span class="px-2 py-1 text-xl bg-gray-200 rounded-md">
+        <i class="bi bi-type-bold"></i>
+      </span>
+      <span class="px-2 py-1 text-xl bg-gray-200 rounded-md">
+        <i class="bi bi-type-underline"></i>
+      </span>
+      <span class="px-2 py-1 text-xl bg-gray-200 rounded-md">
+        <i class="bi bi-type-italic"></i>
+      </span>
+    </div>
 
-      <input v-model="title" type="text" placeholder="Titlu"
-        class="border border-gray-300 p-2 mb-4 rounded-md w-full max-w-xs" />
+    <input v-model="title" type="text" placeholder="Titlu"
+      class="border border-gray-300 p-2 mb-4 rounded-md w-full max-w-xs" />
 
-      <textarea v-model="description" placeholder="Adaugă un text nou..."
-        class="p-2 rounded-md w-full max-w-xs min-h-[30rem] placeholder-gray-400 resize-none"></textarea>
+    <textarea v-model="description" placeholder="Adaugă un text nou..."
+      class="p-2 rounded-md w-full max-w-xs min-h-[30rem] placeholder-gray-400 resize-none"></textarea>
 
-      <button type="submit" class="absolute bottom-4 right-4 bg-brand-olivine text-white px-4 py-2 rounded-full">
-        Adaugǎ
-      </button>
-    </form>
-  </div>
+    <button type="submit" class="absolute bottom-4 right-4 bg-brand-olivine text-white px-4 py-2 rounded-full">
+      Adaugǎ
+    </button>
+  </form>
 </template>
 
 <style scoped></style>
